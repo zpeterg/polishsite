834c6d2568e762b2925f7a687da864cbcd9e4f22
Peter Granderson <solace@fastmail.fm>
2019-04-03 23:57:44 -0500

    build

---
+++ build/precache-manifest.2b847d5eb6c526c3b2d3f16ec110310d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "356a178e6e09fafa1852",
+    "url": "/polishsite/build/static/js/runtime~main.73b91704.js"
+  },
+  {
+    "revision": "ddc41059fc29672bce3c",
+    "url": "/polishsite/build/static/js/main.feba2d43.chunk.js"
+  },
+  {
+    "revision": "188fc7a934d1186bb06a",
+    "url": "/polishsite/build/static/js/2.9f185d4c.chunk.js"
+  },
+  {
+    "revision": "ddc41059fc29672bce3c",
+    "url": "/polishsite/build/static/css/main.f0e6e24f.chunk.css"
+  },
+  {
+    "revision": "b7a327a11a446239e4950c69a76f6ef4",
+    "url": "/polishsite/build/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.758b3def4822341dba514333c82348dc.js"
+  "/polishsite/build/precache-manifest.2b847d5eb6c526c3b2d3f16ec110310d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/polishsite/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
